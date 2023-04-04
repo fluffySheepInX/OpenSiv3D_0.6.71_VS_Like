ca64c7b66fc6d4d1464cc419d2daa8f9710305b8
fluffysheepinx <fluffysheepinx@gmail.com>
2023-04-04 13:18:25 +0900

    構造変更

--- OpenSiv3D_0.6.71_VS_Like/Main.cpp
+++ OpenSiv3D_0.6.71_VS_Like/Main.cpp
@@ -29,6 +29,41 @@ enum GameMode {
 	Exit
 };
 
+// 弾の情報を表す構造体
+struct Bullet
+{
+	/// @brief 中心座標
+	Vec2 pos;
+
+	/// @brief 半径
+	double radius;
+
+	/// @brief 向き（正規化済）
+	Vec2 direction;
+
+	/// @brief 早さ（ピクセル毎秒）
+	double speed;
+
+	/// @brief 色
+	ColorF color;
+
+	/// @brief ダメージ量
+	int32 damage;
+
+	/// @brief 弾の Circle を返す関数
+	/// @return Circle
+	Circle getCircle() const
+	{
+		return Circle{ pos, radius };
+	}
+
+	/// @brief 弾を描画する関数
+	void draw() const
+	{
+		getCircle().draw(color);
+	}
+};
+
 class WindowManager {
 
 public:
@@ -202,26 +237,25 @@ public:
 
 		int height = 100;
 		int height2 = 200;
+		double bgmValue = 5.0;
 		RectF rectText = font(text).region();
-		RectF rect = { (WindowSizeWidth / 2) - (rectText.w / 2), height,rectText.w,60 };
+
+		RectF rect = { (WindowSizeWidth / 2) - (rectText.w / 2) - 150, height,rectText.w,60 };
 		RectF rectText2 = font(textBackMenu).region();
 		RectF rect2 = { (WindowSizeWidth / 2) - (rectText2.w / 2), height2,rectText2.w,60 };
-
 		GameMode ga = Game;
 		while (System::Update())
 		{
 			//draw
 			{
-				rect.draw(Arg::top = ColorF{ 0.5 }, Arg::bottom = ColorF{ 1.0 });
-				font(text).draw((WindowSizeWidth / 2) - (rectText.w / 2), height, ColorF{ 0.25 });
+				//rect.draw(Arg::top = ColorF{ 0.5 }, Arg::bottom = ColorF{ 1.0 });
+				font(text).draw((WindowSizeWidth / 2) - (rectText.w / 2) - 150, height, ColorF{ 0.25 });
+				SimpleGUI::Slider(U"{:.2f}"_fmt(bgmValue), bgmValue, 0.0, 10.0, Vec2{ (WindowSizeWidth / 2) - (rectText.w / 2) + 150, height });
 				rect2.draw(Arg::top = ColorF{ 0.5 }, Arg::bottom = ColorF{ 1.0 });
 				font(textBackMenu).draw((WindowSizeWidth / 2) - (rectText2.w / 2), height2, ColorF{ 0.25 });
-			}
-
-			if (rect.leftClicked())
-			{
 
 			}
+
 			if (rect2.leftClicked())
 			{
 				ga = Menu;
@@ -248,6 +282,8 @@ public:
 		bool isPlayerFacingRight = true;
 		// プレイヤーの位置
 		Vec2 playerPos(WindowSizeWidth / 2, WindowSizeHeight / 2);
+		Vec2 centerPos(WindowSizeWidth / 2, WindowSizeHeight / 2);
+		Array<Bullet> bullets;
 
 		while (System::Update())
 		{
@@ -260,6 +296,11 @@ public:
 
 				// プレイヤーを描く | Draw the player
 				DrawPlayer(texturePlayer, isPlayerFacingRight);
+
+				// 弾を描画する
+				DrawBullets(bullets);
+				// 弾情報を更新する
+				UpdateBullets(bullets, Scene::DeltaTime());
 			}
 
 			{
@@ -291,6 +332,16 @@ public:
 				{
 					// プレイヤーが上に移動する | Player moves up
 					playerPos.y = (playerPos.y + speed * Scene::DeltaTime());
+				}
+
+				// マウスの左ボタンが押されていたら
+				if (MouseL.pressed())
+				{
+					// マウスの位置を取得する | Gets the mouse position
+					Vec2 mousePos = Cursor::Pos();
+
+					// マウスの位置に向かって弾を発射する// Fires the bullet towards the mouse position
+					FireBullet(centerPos, mousePos, bullets);
 				}
 			}
 		}
@@ -407,6 +458,42 @@ private:
 		return Menu;
 	}
 
+	/// @brief 弾の移動と削除を行います。
+	/// @param bullets 弾の配列
+	/// @param deltaTime 経過時間
+	void UpdateBullets(Array<Bullet>& bullets, double deltaTime)
+	{
+		for (auto& bullet : bullets)
+		{
+			bullet.pos += (bullet.direction * bullet.speed * deltaTime);
+		}
+
+		// 画面外の弾を削除（方法 1）
+		bullets.remove_if([](const Bullet& b)
+			{
+					return (not b.getCircle().intersects(Scene::Rect()));
+			});
+	}
+	// Fires the bullet towards the mouse position
+	void FireBullet(Vec2 playerPos, Vec2 mousePos, Array<Bullet>& bullets)
+	{
+		Bullet bullet;
+		bullet.pos = playerPos;
+		bullet.direction = (mousePos - playerPos).normalized();
+		bullet.speed = 200;
+		bullet.radius = 10.0;
+		bullet.color = Palette::Red;
+		bullet.damage = 10;
+		bullets.push_back(bullet);
+	}
+	// 弾を描画する関数
+	void DrawBullets(Array<Bullet>& bullets)
+	{
+		for (const auto& bullet : bullets)
+		{
+			bullet.draw();
+		}
+	}
 	// マップを描画する
 	void DrawMap(Vec2 playerPos, Texture mapTexture, Font font)
 	{
@@ -491,104 +578,4 @@ void Main()
 			break;
 		}
 	}
-
-	// 画像ファイルからテクスチャを作成する | Create a texture from an image file
-	const Texture texture{ U"example/windmill.png" };
-
-	// 絵文字からテクスチャを作成する | Create a texture from an emoji
-	const Texture emoji{ U"🦖"_emoji };
-
-	// 太文字のフォントを作成する | Create a bold font with MSDF method
-	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
-
-	// テキストに含まれる絵文字のためのフォントを作成し、font に追加する | Create a font for emojis in text and add it to font as a fallback
-	const Font emojiFont{ 48, Typeface::ColorEmoji };
-	font.addFallback(emojiFont);
-
-	// ボタンを押した回数 | Number of button presses
-	int32 count = 0;
-
-	// チェックボックスの状態 | Checkbox state
-	bool checked = false;
-
-	// プレイヤーの移動スピード | Player's movement speed
-	double speed = 200.0;
-
-	// プレイヤーの X 座標 | Player's X position
-	double playerPosX = 400;
-
-	// プレイヤーが右を向いているか | Whether player is facing right
-	bool isPlayerFacingRight = true;
-
-	//System::SetTerminationTriggers(UserAction::CloseButtonClicked);
-
-	while (System::Update())
-	{
-		// テクスチャを描く | Draw the texture
-		texture.draw(20, 20);
-
-		// テキストを描く | Draw text
-		font(U"Hello, Siv3D!🎮").draw(64, Vec2{ 20, 340 }, ColorF{ 0.2, 0.4, 0.8 });
-
-		// 指定した範囲内にテキストを描く | Draw text within a specified area
-		font(U"Siv3D (シブスリーディー) は、ゲームやアプリを楽しく簡単な C++ コードで開発できるフレームワークです。")
-			.draw(18, Rect{ 20, 430, 480, 200 }, Palette::Black);
-
-		// 長方形を描く | Draw a rectangle
-		Rect{ 540, 20, 80, 80 }.draw();
-
-		// 角丸長方形を描く | Draw a rounded rectangle
-		RoundRect{ 680, 20, 80, 200, 20 }.draw(ColorF{ 0.0, 0.4, 0.6 });
-
-		// 円を描く | Draw a circle
-		Circle{ 580, 180, 40 }.draw(Palette::Seagreen);
-
-		// 矢印を描く | Draw an arrow
-		Line{ 540, 330, 760, 260 }.drawArrow(8, SizeF{ 20, 20 }, ColorF{ 0.4 });
-
-		// 半透明の円を描く | Draw a semi-transparent circle
-		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1.0, 0.0, 0.0, 0.5 });
-
-		// ボタン | Button
-		if (SimpleGUI::Button(U"count: {}"_fmt(count), Vec2{ 520, 370 }, 120, (checked == false)))
-		{
-			// カウントを増やす | Increase the count
-			++count;
-		}
-
-		// チェックボックス | Checkbox
-		SimpleGUI::CheckBox(checked, U"Lock \U000F033E", Vec2{ 660, 370 }, 120);
-
-		// スライダー | Slider
-		SimpleGUI::Slider(U"speed: {:.1f}"_fmt(speed), speed, 100, 400, Vec2{ 520, 420 }, 140, 120);
-
-		// 左キーが押されていたら | If left key is pressed
-		if (KeyLeft.pressed())
-		{
-			// プレイヤーが左に移動する | Player moves left
-			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), 60.0);
-			isPlayerFacingRight = false;
-		}
-
-		// 右キーが押されていたら | If right key is pressed
-		if (KeyRight.pressed())
-		{
-			// プレイヤーが右に移動する | Player moves right
-			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), 740.0);
-			isPlayerFacingRight = true;
-		}
-
-		// プレイヤーを描く | Draw the player
-		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, 540);
-	}
 }
-
-//
-// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
-//
-// - Release ビルド: 最大限の最適化でビルドします。
-//
-// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
-//
-// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要な場合があります。
-//
